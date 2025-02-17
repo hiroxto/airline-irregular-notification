4dc1f6164bf3fbf1a82482955c9326645b2fcc7d
Hiroto Kitazawa <hiroxto@gmail.com>
2025-02-17 12:53:14 +0900

    通知に取得元のURLを含める

--- app.ts
+++ app.ts
@@ -83,7 +83,7 @@ function getUpdateTime(html: string): string {
 }
 
 function formatMessage(flightInfos: FlightInfo[], updateTime: string): { text: string; blocks: (Block | KnownBlock)[] } {
-    const headerText = '*特別な取り扱いの一覧 / ANA* @here\n';
+    const headerText = `*特別な取り扱いの一覧 / <${ANA_URL}|ANA> @here*\n`;
     const blocks: (Block | KnownBlock)[] = [
         {
             type: 'section',
